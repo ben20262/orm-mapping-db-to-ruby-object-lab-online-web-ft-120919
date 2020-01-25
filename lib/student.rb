4f03743cda0341974218a7f5bd3dcd8ebddc219a
Benjamin Small <ben20262@aim.com>
2020-01-25 21:06:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,7 +61,7 @@ class Student
       WHERE students.grade = 10
       LIMIT ?
     SQL
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql, num).map do |row|
       self.new_from_db(row)
     end
   end
