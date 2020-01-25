259fc7b8d586844e1855e9540deeac187e81f1b4
Benjamin Small <ben20262@aim.com>
2020-01-25 21:32:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,6 +66,16 @@ class Student
     end
   end
 
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE students.grade = 10
+      LIMIT 1
+    SQL
+    row = DB[:conn].execute(sql)
+    self.new_from_db(row)
+  end
 
   def save
     sql = <<-SQL
