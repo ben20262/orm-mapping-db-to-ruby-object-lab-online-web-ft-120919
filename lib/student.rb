e93afaa0d852076ab4f86f079654a88f03f55d14
Benjamin Small <ben20262@aim.com>
2020-01-25 20:56:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,9 +36,18 @@ class Student
 
   def self.all_students_in_grade_9
     sql = <<-SQL
-    SELECT *
-    FROM students
-    WHERE students.grade = 9
+      SELECT *
+      FROM students
+      WHERE students.grade = 9
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE stuents.grade <> 12
     SQL
     DB[:conn].execute(sql)
   end
